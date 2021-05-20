b1df65b6fbe8d3de50ef24e32a4f66c89f13d9cb
JustinMethorstStudent <400022278@st.roc.a12.nl>
2021-05-20 13:41:40 +0200

    still initial

---
+++ homePage.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Netflix Home</title>
+</head>
+<body>
+
+<div class="topnav">
+    <a href="homePage.php"><img src="images/Netflix_Logo_RGB.png" alt="Netflix"></a>
+    <div class="myLinks">
+        <a href="homePage.php">Home</a>
+        <a href="seriesPage.php">Series</a>
+        <a href="newAndPopularPage.php">New And Popular</a>
+        <a href="myListPage.php">My List</a>
+    </div>
+</div>
+
+</body>
+</html>
