66fafb9a549ece3f94b094082b1fddf8e85e8ec8
Mariannedg <78961612+Mariannedg@users.noreply.github.com>
2021-05-27 17:14:18 +0200

    Update db_connection.php

--- db_connection.php
+++ db_connection.php
@@ -1,11 +1,14 @@
 <?php
 
-$host = 'localhost';
-$dbname = 'netflix';
-$user = 'root';
-$pass = '';
+function getDbConnection() {
+    $host = 'localhost';
+    $dbname = 'netflix';
+    $user = 'root';
+    $pass = '';
 
-$dbh = new PDO('mysql:host='.$host.';dbname='.$dbname, $user, $pass);
-$dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_WARNING);
+    $dbh = new PDO('mysql:host='.$host.';dbname='.$dbname, $user, $pass);
+    $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_WARNING);
 
+    return $dbh;
+}
 ?>
